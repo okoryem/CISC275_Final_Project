cc03eab9cf92cbcad07a3def577062e930201067
okoryem <okoryem@udel.edu>
2025-03-28 13:54:38 -0400

    (Task 16) Added Basic Assesment Career Option

---
+++ src/components/BasicAssesmentOption.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+
+export function BasicOption(): React.JSX.Element {
+
+    return (
+        <div>
+            <h4>Not sure what career is right for you? Take our quick quiz!</h4>
+            <Button onClick={() => {console.log("Basic Assesment Option Button Pressed. Temporary Function")}}>
+                Find Your Career Path
+            </Button>
+        </div>
+    );
+}

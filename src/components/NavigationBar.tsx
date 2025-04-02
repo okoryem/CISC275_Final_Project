9cbfe0a733e2241609fd88ad2a81b0d57a3fde7e
Andrew Altmann <altmanna@udel.edu>
2025-04-01 22:16:09 -0400

    navbar design edited

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import "./NavigationBar.css"; 
 
-export function NavigationBar(): React.JSX.Element {
+export function NavigationBar() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+    <Navbar className="navbar-custom" variant="dark" expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand href="/">Career Quiz</Navbar.Brand>
+        {}
+        <Navbar.Brand href="/" className="nav-brand">Career Helpi</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto nav-links">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/basic">Basic Quiz</Nav.Link>
-            <Nav.Link href="/detailed">Detailed Quiz</Nav.Link>
-          </Nav>
-          <Nav>
+            <Nav.Link href="/basic-quiz">Basic Quiz</Nav.Link>
+            <Nav.Link href="/detailed-quiz">Detailed Quiz</Nav.Link>
             <Nav.Link href="/login">Login/Sign up</Nav.Link>
           </Nav>
         </Navbar.Collapse>

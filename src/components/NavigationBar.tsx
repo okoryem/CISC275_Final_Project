c6efd48ed0a7c028c013f595578d25bfcdc91a3e
Andrew Altmann <altmanna@udel.edu>
2025-04-07 21:51:34 -0400

    added quiz pages and buttons that lead to them

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import "./NavigationBar.css"; 
 
 export function NavigationBar() {
   return (
     <Navbar className="navbar-custom" variant="dark" expand="lg" fixed="top">
       <Container>
-        {}
-        <Navbar.Brand href="/" className="nav-brand">Career Helpi</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/" className="nav-brand">
+          Ultimate Career Quiz
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto nav-links">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/basic-quiz">Basic Quiz</Nav.Link>
-            <Nav.Link href="/detailed-quiz">Detailed Quiz</Nav.Link>
-            <Nav.Link href="/login">Login/Sign up</Nav.Link>
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/basic">Basic Quiz</Nav.Link>
+            <Nav.Link as={NavLink} to="/detailed">Detailed Quiz</Nav.Link>
+            <Nav.Link as={NavLink} to="/login">Login/Sign up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>

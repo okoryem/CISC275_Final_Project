39119b047bfc95587033013eb4221960e2f08ddc
Andrew Altmann <altmanna@udel.edu>
2025-04-14 15:40:13 -0400

    API Key submission doesn't lead to blank page

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
   //sets the local storage item to the api key the user inputed
   function handleSubmit() {
     localStorage.setItem(saveKeyData, JSON.stringify(key));
-    window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
+    //window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
   }
 
   //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
@@ -54,7 +54,7 @@ function App() {
 
         <footer className="footer">
           <hr />
-          <Form className="api-form">
+          <Form className="api-form" onSubmit={handleSubmit}>
             <Form.Label>API Key:</Form.Label>
             <Form.Control
               type="password"

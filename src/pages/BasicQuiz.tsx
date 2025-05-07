539a9006d166221e0b4640b0aed171a8762d997c
Andrew Altmann <altmanna@udel.edu>
2025-05-07 00:07:19 -0400

    remove answers in basic quiz

--- src/pages/BasicQuiz.tsx
+++ src/pages/BasicQuiz.tsx
@@ -6,12 +6,22 @@ import { ProgressBar } from "../components/ProgressBar";
 import { basicQuestions } from "./BasicQuizData";
 import OpenAI from "openai";
 
-export function BasicQuiz({apiKey, setResponse}: { apiKey: string; setResponse: React.Dispatch<React.SetStateAction<string>> }) {
+export function BasicQuiz({
+  apiKey,
+  setResponse,
+}: {
+  apiKey: string;
+  setResponse: React.Dispatch<React.SetStateAction<string>>;
+}) {
   
-  //This array below is where the answers for each question will be stored e.g (answersGrid[0] = ANSWER FOR FIRST QUESTION)
-  const [answersGrid, setAnswersGrid] = useState<(string[])>(["","","","","","","","","",""]);
-  const [barPercentage, setBarPercentage] = useState<number>(0); //Bar fills up based on integer value e.g (0-100)
-  const [showResults, setShowResults] = useState(false);
+  //THIS ARRAY BELOW IS WHERE THE ANSWERS ARE STORED FOR EACH QUESTION
+  const [answersGrid, setAnswersGrid] = useState<(string[])>([
+    "", "", "", "", "", "", "", "", "", ""
+  ]);
+
+  //Bar fills up at the top of screen based on integer value e.g (0-100)
+  const [barPercentage, setBarPercentage] = useState<number>(0); 
+
   const navigate = useNavigate();
 
   const openai = new OpenAI({
@@ -37,14 +47,16 @@ export function BasicQuiz({apiKey, setResponse}: { apiKey: string; setResponse: 
         },
         {
           role: "user",
-          content: `I just took a Basic career quiz (mention in your response that these are the results from a Basic career quiz). These are the questions and their respective answers: ${basicQuestions.map(
-            (a, i) =>
-              "The question was " +
-              a.question +
-              " and my answer was: " +
-              answersGrid[i] +
-              ". "
-          ).join("")}`,
+          content: `I just took a Basic career quiz (mention in your response that these are the results from a Basic career quiz). These are the questions and their respective answers: ${basicQuestions
+            .map(
+              (a, i) =>
+                "The question was " +
+                a.question +
+                " and my answer was: " +
+                answersGrid[i] +
+                ". "
+            )
+            .join("")}`,
         },
       ],
     });
@@ -55,48 +67,38 @@ export function BasicQuiz({apiKey, setResponse}: { apiKey: string; setResponse: 
 
     // Navigate to the results page AFTER getting the response
     navigate("/results");
-  } 
+  }
 
   return (
-    <div >
+    <div>
+      <h2>Welcome to the Basic Career Quiz</h2>
       <div className="progress-bar-container">
-      Completion %
-      <ProgressBar value={barPercentage}></ProgressBar>
+        Completion %
+        <ProgressBar value={barPercentage}></ProgressBar>
       </div>
 
-      <div className ="question-block-group">
+      <div className="question-block-group">
         Please Answer The Questions Below:
-      {basicQuestions.map((prop) =>  <QuestionTemplate id={prop.id} question={prop.question} options={prop.options} answersGrid={answersGrid} setAnswersGrid={setAnswersGrid} barPercentage={barPercentage} setBarPercentage={setBarPercentage} ></QuestionTemplate>)}
+        {basicQuestions.map((prop) => (
+          <QuestionTemplate
+            key={prop.id}
+            id={prop.id}
+            question={prop.question}
+            options={prop.options}
+            answersGrid={answersGrid}
+            setAnswersGrid={setAnswersGrid}
+            barPercentage={barPercentage}
+            setBarPercentage={setBarPercentage}
+          />
+        ))}
       </div>
 
       <button
         className="submit-button"
-        onClick={() => {
-          setShowResults(true) 
-          displayResponse()
-          }
-        }
-        
+        onClick={displayResponse}
       >
-      
         Generate Career Report
       </button>
-
-      {showResults && (
-        <div className="results">
-          <h3>Your Answers:</h3>
-          <ul>
-            {basicQuestions.map((item, i) => (
-              <li key={i}>
-                <strong>{basicQuestions[i].question}</strong><br />
-                Answer: {answersGrid[i]}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-        
     </div>
-);
-
+  );
 }
